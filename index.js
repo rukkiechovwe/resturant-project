46e4fe0a528a2bc80cb9f3eeddba376976179554
rukkiechovwe <rukkiechovwe@gmail.com>
2020-11-18 00:04:52 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// var pointer = 0;
+// let moveFoward = ()=>{
+// 	var figures = document.getElementsByClassName("categories-content");
+// 	for (var i = 0; i < figures.length; i++) {
+// 		figures[i].style.display = 'none';
+// 	}
+// 	pointer++
+// 	if(pointer > figures.length){
+// 		pointer=1
+// 	}
+// 	figures[pointer-1].style.display = "flex"
+// 	setTimeout(moveFoward,8000)
+// }
+// moveFoward()
+
+// var pointer = 0;
+// let moveFoward = ()=>{
+// 	var figures = document.getElementsByTagName("figure");
+// 	for (var i = 0; i < figures.length; i++) {
+// 		if (figures[i].className = "visible") {
+// 			figures[i].className = "hidden"
+// 		}
+// 	}
+// 	pointer++
+// 	if(pointer > figures.length){
+// 		pointer = 1
+// 	}
+
+// 	if (figures[pointer-1].className = "hidden") {
+// 		figures[pointer-1].className = "visible"
+// 	}
+// 	setTimeout(moveFoward,4000)
+// }
+// moveFoward()
